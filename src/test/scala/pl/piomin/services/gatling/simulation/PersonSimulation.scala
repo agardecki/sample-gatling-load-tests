8c6d7bd502bede6df8277adb91a4db602d6ff7be
Andrzej Gardecki <a.gardecki@gmail.com>
2019-03-13 00:11:38 +0100

    new simulation

--- src/test/scala/pl/piomin/services/gatling/simulation/PersonSimulation.scala
+++ src/test/scala/pl/piomin/services/gatling/simulation/PersonSimulation.scala
@@ -1,11 +1,11 @@
 package pl.piomin.services.gatling.simulation
 
-import io.gatling.core.Predef.{Simulation, rampUsers, atOnceUsers}
+import io.gatling.core.Predef._
 import pl.piomin.services.gatling.config.Http
 import pl.piomin.services.gatling.scenario.PersonScenario
 
 import scala.concurrent.duration._
 
 class PersonSimulation extends Simulation {
-  setUp(PersonScenario.scnPostPersonRepeat1000.inject(atOnceUsers(10))).maxDuration(1 minutes).protocols(Http.httpProtocol)
+  setUp(PersonScenario.scnPostPersonRepeat1000.inject(rampUsers(10) during (10 seconds))).maxDuration(1 minutes).protocols(Http.httpProtocol)
 }
